71dcd7b9ec36aef2df70ced506a66c64d2e600c7
Harry Garrood <hdgarrood@gmail.com>
2013-01-09 22:50:55 +0000

    fix functional test

--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -1,26 +1,29 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class IssuesControllerTest < ActionController::TestCase
-  fixtures :custom_fields,
+  fixtures :custom_fields_projects,
+    :custom_fields_trackers,
+    :custom_fields,
     :custom_values,
+    :enabled_modules,
+    :enumerations,
     :issue_categories,
     :issue_statuses,
     :issues,
     :member_roles,
     :members,
+    :projects_trackers,
     :projects,
     :release_notes,
     :roles,
-    :users
-
-  def setup
-    # run as redmine admin
-  end
+    :trackers,
+    :users,
+    :workflows
 
   test 'error is shown on issues#show when issue custom field is not set up' do
-    @request.session[:user_id] = 1
     get :show, :id => '1'
     assert_response :success
-    assert_select 'div.flash.error', I18n.l(:failed_find_issue_custom_field)
+    assert_select 'div.flash.error',
+      :text => I18n.t(:failed_find_issue_custom_field)
   end
 end

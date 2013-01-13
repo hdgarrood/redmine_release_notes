3903c037f3a430e41d380e7988296886d422d35a
Harry Garrood <hdgarrood@gmail.com>
2013-01-13 15:32:48 +0000

    add another test

--- test/functional/issue_hook_test.rb
+++ test/functional/issue_hook_test.rb
@@ -1,6 +1,8 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class IssueHookTest < ActionController::TestCase
+  # TODO: sort out transactional tests
+
   fixtures :custom_fields_projects,
     :custom_fields_trackers,
     :custom_fields,
@@ -30,8 +32,23 @@ class IssueHookTest < ActionController::TestCase
 
     assert_response :success
     assert_select 'div.flash.error', false
-    assert_select 'p#release_notes',
+    assert_select 'div#release_notes>p',
       :text => /Recipes may now be printed/
+  end
+
+  test 'release notes not displayed if module is disabled' do
+    proj = Project.find(1)
+    proj.transaction do
+      proj.enabled_modules.delete(EnabledModule.find_by_name(:release_notes))
+
+      get :show, :id => '1'
+      assert_response :success
+      assert_select 'div.flash.error', false
+      assert_select 'div#release_notes>p', false
+
+      # roll back at the end to undo changes
+      raise ActiveRecord::Rollback
+    end
   end
 
   test 'error is shown on issues#show when issue custom field is not set up' do

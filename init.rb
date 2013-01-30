e07e0f518576eb8abba1f32880902f6c7045ae46
Harry Garrood <hdgarrood@gmail.com>
2013-01-30 17:50:53 +0000

    update version

--- init.rb
+++ init.rb
@@ -32,7 +32,7 @@ Redmine::Plugin.register :redmine_release_notes do
   name 'Redmine release notes plugin'
   author 'Harry Garrood'
   description 'A plugin for managing release notes.'
-  version '1.3.0'
+  version '1.3.0-pre'
   author_url 'https://github.com/hdgarrood'
   requires_redmine :version_or_higher => '2.0.0'
 

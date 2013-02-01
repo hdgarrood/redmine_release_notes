b2a038bbb4d3e55a62a08e7f8ff10983f1c750f9
Harry Garrood <hdgarrood@gmail.com>
2013-02-01 10:45:44 +0000

    allow release notes query column to be groupable and sortable

--- app/models/release_notes_query_column.rb
+++ app/models/release_notes_query_column.rb
@@ -1,13 +1,11 @@
 class ReleaseNotesQueryColumn < QueryColumn
   def initialize
-    self.name = :release_notes
-    self.sortable = false
-    self.groupable = false#'release_notes'
-    @inline = true
-    @caption_key = 'release_notes.title_plural'
+    super(:release_notes,
+          :sortable => 'rn.status',
+          :groupable => 'rn.status',
+          :caption => 'release_notes.title_plural')
   end
 
-  # not sure if this is ever used
   def value(object)
     I18n.t(object.release_note.status, :scope => 'release_notes.status')
   end

385968b5866739542737116f1dfec855b2d686bc
Harry Garrood <hdgarrood@gmail.com>
2013-02-04 11:12:06 +0000

    add test for query patch

---
+++ test/unit/query_patch_test.rb
@@ -0,0 +1,51 @@
+require File.dirname(__FILE__) + '/../test_helper.rb'
+
+class QueryPatchTest < ActiveSupport::TestCase
+  # create a project with 3 issues with release notes todo, 4 done, and 5
+  # not required
+  def make_a_project_with_some_issues_and_release_notes
+    project = FactoryGirl.create(:project,
+                                 :enabled_module_names => %w(issue_tracking
+                                                             release_notes))
+    [[3, 'todo'], [4, 'done'], [5, 'not_required']].map do |n, status|
+      n.times do
+        issue = FactoryGirl.create(:issue, :project => project)
+        issue.build_release_note
+        issue.release_note.status = status
+        issue.release_note.text = 'LULZ' # so that it's valid
+        issue.save!
+      end
+    end
+    project
+  end
+
+  test 'release notes filter available when project has release notes' +
+  ' enabled' do
+    project = FactoryGirl.create(:project,
+                                 :enabled_module_names => %w(issue_tracking
+                                                             release_notes))
+    query = IssueQuery.new(:project => project)
+
+    assert query.available_filters.include?("release_notes")
+  end
+
+  test 'release notes filter not available when no project' do
+    assert !IssueQuery.new.available_filters.include?('release_notes')
+  end
+
+  test 'release notes filter not available when release notes module ' +
+  'disabled' do
+    project = FactoryGirl.create(:project,
+                                 :enabled_module_names => %w(issue_tracking))
+    query = IssueQuery.new(:project => project)
+
+    assert !query.available_filters.include?("release_notes")
+  end
+
+  test 'issue_count returns correct value' do
+    project = make_a_project_with_some_issues_and_release_notes
+    query = IssueQuery.new(:project => project)
+
+    assert_equal 12, query.issue_count
+  end
+end

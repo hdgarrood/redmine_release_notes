6aef6fda48bc0c8b984bb5800ff1b7e3d212905c
Harry Garrood <hdgarrood@gmail.com>
2013-02-18 01:37:58 +0000

    fix GPLs (#57)

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
-# Copyright © 2012  Harry Garrood
+# Copyright (C) 2012-2013 Harry Garrood
 # This file is a part of redmine_release_notes.
 
 # redmine_release_notes is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 
-# redmine_release_notes is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# redmine_release_notes is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
 
-# You should have received a copy of the GNU General Public License
-# along with redmine_release_notes.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# redmine_release_notes. If not, see <http://www.gnu.org/licenses/>.
 
 RedmineApp::Application.routes.draw do
   resources :release_notes,
--- lib/tasks/redmine.rake
+++ lib/tasks/redmine.rake
@@ -1,10 +1,18 @@
-# differences to standard Redmine plugin tests:
-#   executes db:test:purge instead of db:test:prepare
-#   verbose is set to false
-#   only does release notes plugin
-#
-# also contains redmine:plugins:release_notes:load_default_data for creating
-# the standard ReleaseNotesFormats.
+# Copyright (C) 2012-2013 Harry Garrood
+# This file is a part of redmine_release_notes.
+
+# redmine_release_notes is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+
+# redmine_release_notes is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+
+# You should have received a copy of the GNU General Public License along with
+# redmine_release_notes. If not, see <http://www.gnu.org/licenses/>.
 
 require 'yaml'
 
